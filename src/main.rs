9eb3614b532525814670d5d033bab352223b1d50
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-25 14:19:20 -0600

    creating and understanding the hashmap type. Understanding how to use it, how to import it, how it works, understanding key-value relationship and how its viable. :)

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    let mut menu: HashMap<String, f64> = HashMap::new();
+
+    menu.insert("Salmon".to_string(), 32.99);
+    menu.insert("Steak".to_string(), 42.99);
+    menu.insert("Lobster".to_string(), 32.99);
+
+    println!("{menu:#?}");
+
+    // let mut country_capital: HashMap<&str, &str> = HashMap::new();
+    let mut country_capital = HashMap::<&str, &str>::new();
+
+    country_capital.insert("France", "Paris");
+    country_capital.insert("Germany", "Berlin");
+    country_capital.insert("Austin", "Texas");
+
+    println!("{country_capital:#?}");
+}
