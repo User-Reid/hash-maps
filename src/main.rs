bf6b886249584824d4199dee231f0b891a16bfff
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-27 15:39:56 -0600

    understanding various different methods that can be used on Hashsets :)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 use std::collections::HashSet;
 
 fn main() {
-    let mut concert_que: HashSet<&str> = HashSet::new();
-    println!("{concert_que:#?}");
+    let mut concert_que: HashSet<&str> = HashSet::new(); // Reid, Rae
+    let mut movie_que: HashSet<&str> = HashSet::new(); // Reid, Troy
+
     concert_que.insert("Reid");
     concert_que.insert("Rae");
-    println!("{concert_que:#?}");
-    println!("{:#?}", concert_que.len());
 
-    concert_que.insert("Reid");
-    println!("{concert_que:#?}");
+    movie_que.insert("Reid");
+    movie_que.insert("Troy");
 
-    println!("{}", concert_que.remove("Rae"));
-    println!("{}", concert_que.remove("Tol"));
-    println!("{:#?}", concert_que);
+    println!("{:#?}", concert_que.union(&movie_que));
+    println!("{:#?}", concert_que.difference(&movie_que));
+    println!("{:#?}", movie_que.difference(&concert_que));
 
-    println!("{:#?}", concert_que.get("Reid").unwrap());
+    println!("{:#?}", movie_que.symmetric_difference(&concert_que));
+
+    println!("{:#?}", movie_que.is_disjoint(&concert_que));
+
+    println!("{:#?}", movie_que.is_subset(&concert_que));
+
+    let mut attendies: HashSet<&str> = HashSet::new();
+    attendies.insert("Reid");
+    println!("{:#?}", concert_que.is_subset(&attendies));
 }
